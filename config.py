1b034d326ecd69fa97b692b549c8cab1612da70f
vitheshshetty00 <vitheshshetty23@gmail.com>
2023-09-21 21:41:04 +0530

    first

---
+++ config.py
@@ -0,0 +1,20 @@
+elasticsearch_host = "localhost"
+elasticsearch_port = 9200
+elasticsearch_user = "elastic"
+elasticsearch_password = "changeme"
+consumer_key = "4ACNpRClhunNOFP1eJAGFEY13"
+consumer_secret = "N27GDz3wapsGr87UtqiAwFh2ImcuZ04MHRjFL0Pckov98tZHba"
+access_token = "1702692370280820736-xfBzPHALad0nxNcyOqHtXtt31LQxnt"
+access_token_secret = "cwxVrxk19zG0K1C6VM9ICDGhOKcyexRJ9xXWHe5gqel22"
+nltk_tokens_required = ("neuralink", "solar", "tesla", "@tesla", "#tesla", "tesla", "tsla", "#tsla", "elonmusk", "elon", "musk", "spacex", "starlink")
+nltk_min_tokens = 1
+nltk_tokens_ignored = ("win", "giveaway")
+twitter_feeds = ["@elonmusk", "@cnbc", "@benzinga", "@stockwits",
+                 "@Newsweek", "@WashingtonPost", "@breakoutstocks", "@bespokeinvest",
+                 "@WSJMarkets", "@stephanie_link", "@nytimesbusiness", "@IBDinvestors",
+                 "@WSJDealJournal", "@jimcramer", "@TheStalwart", "@TruthGundlach",
+                 "@Carl_C_Icahn", "@ReformedBroker", "@bespokeinvest", "@stlouisfed",
+                 "@muddywatersre", "@mcuban", "@AswathDamodaran", "@elerianm",
+                 "@MorganStanley", "@ianbremmer", "@GoldmanSachs", "@Wu_Tang_Finance",
+                 "@Schuldensuehner", "@NorthmanTrader", "@Frances_Coppola", "@bySamRo",
+                 "@BuzzFeed","@nytimes"]
